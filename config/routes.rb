43daac02af90fb8ab3d7706bee1212411d9baf24
wardou2 <ward92@gmail.com>
2019-07-14 18:39:13 -0700

    changed login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   put '/api/v1/users/find', to: 'api/v1/users#findUser'
-  post '/login', to: 'application#log_in'
+  post 'api/v1/login', to: 'application#log_in'
   put '/api/v1/tracks/get_tracks', to: 'api/v1/tracks#getTracks'
   post '/api/v1/init', to: 'application#init'
   post '/api/v1/tracks/init', to: 'api/v1/tracks#multiple_track_new'
